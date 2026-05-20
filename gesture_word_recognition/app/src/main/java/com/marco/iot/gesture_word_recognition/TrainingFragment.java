77adfbad260e43712d591a6a1b12e9ddffd584ba
Marco Morandi <marcomorandi3005@gmail.com>
2026-05-20 12:34:05 +0200

    Add TrainingFragment java file
    Primitive structure of the fragment has been defined, listeners still
    have to be set.
    xml has been modified in order to add two textviews to display recording
    results.

---
+++ gesture_word_recognition/app/src/main/java/com/marco/iot/gesture_word_recognition/TrainingFragment.java
@@ -0,0 +1,57 @@
+package com.marco.iot.gesture_word_recognition;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TrainingFragment extends Fragment {
+    private final String TAG = "TrainingFragment";
+    private Button bttTrainFragWordRec, bttTrainFragGestureRec;
+    private TextView tvWordRecordDone, tvGestureRecordDone;
+
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+
+        return inflater.inflate(R.layout.training_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Log.i(TAG, "onViewCreated");
+
+        bttTrainFragWordRec = view.findViewById(R.id.bttTrainFragWordRec);
+        bttTrainFragGestureRec = view.findViewById(R.id.bttTrainFragGestureRec);
+        tvWordRecordDone = view.findViewById(R.id.tvWordRecordDone);
+        tvGestureRecordDone = view.findViewById(R.id.tvGestureRecordDone);
+
+        
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+}
