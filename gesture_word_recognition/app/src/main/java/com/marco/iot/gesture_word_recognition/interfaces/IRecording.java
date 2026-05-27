8ac544778c434c470436b838015e287af3d64a61
Igor Coppola <igor.coppola0@gmail.com>
2026-05-27 10:58:52 +0200

    Add audio recording functionality
    The Recorder class and IRecording interface have been defined to handle audio capture in a background thread. The implementation includes methods for initializing, starting, and stopping recordings, with the captured data returned via a callback to the main thread. The AndroidManifest has been updated to include the necessary RECORD_AUDIO permission.

---
+++ gesture_word_recognition/app/src/main/java/com/marco/iot/gesture_word_recognition/interfaces/IRecording.java
@@ -0,0 +1,5 @@
+package com.marco.iot.gesture_word_recognition.interfaces;
+
+public interface IRecording {
+    public void onRecordingDone(short[] audioData);
+}
---
+++ gesture_word_recognition/app/src/main/java/com/marco/iot/gesture_word_recognition/recorder/Recorder.java
@@ -0,0 +1,90 @@
+package com.marco.iot.gesture_word_recognition.recorder;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+import com.marco.iot.gesture_word_recognition.interfaces.IRecording;
+
+import java.util.ArrayList;
+
+public class Recorder {
+    private final String TAG = "Recorder";
+
+    private int fsInHz;
+    private int nSamples;
+    private int recordingLengthInSec;
+
+    private short[] audioData;
+
+    private AudioRecord audioRecord = null;
+
+    private Context context;
+    private IRecording iRecording;
+
+
+    public Recorder(Context context, int fsInHz, int recordingLengthInSec) {
+        Log.i(TAG, "Recorder()");
+
+        this.fsInHz = fsInHz;
+        this.recordingLengthInSec = recordingLengthInSec;
+
+        this.nSamples = fsInHz * this.recordingLengthInSec;
+
+        this.context = context;
+        iRecording = (IRecording) context;
+    }
+
+    public void start() {
+        new Thread( () -> {
+
+            initRecorder();
+            doRecording();
+            releaseRecorder();
+
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(() -> iRecording.onRecordingDone(audioData));
+
+        }).start();
+    }
+
+    public void stop() {
+        audioRecord.stop();
+        releaseRecorder();
+    }
+
+    private void initRecorder() {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, fsInHz, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, 2 * nSamples);
+        audioData = new short[nSamples];
+    }
+
+    private void releaseRecorder() {
+        audioRecord.release();
+        audioRecord = null;
+    }
+
+
+
+    private void doRecording() {
+        Log.i(TAG, "Start recording...");
+
+        audioRecord.startRecording();
+        audioRecord.read(audioData, 0, nSamples);
+
+        Log.i(TAG, "Recording done!");
+    }
+
+}
