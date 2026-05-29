7eb962d032930cf3b7305cf7630fba5c8083b442
Marco Morandi <marcomorandi3005@gmail.com>
2026-05-29 18:02:47 +0200

    Commit for merge

--- gesture_word_recognition/app/src/main/java/com/marco/iot/gesture_word_recognition/accessManager/WordChecker.java
+++ gesture_word_recognition/app/src/main/java/com/marco/iot/gesture_word_recognition/accessManager/WordChecker.java
@@ -9,7 +9,7 @@ public class WordChecker {
     private final String TAG = "WordChecker";
     private DTW dtw;
 
-    private static final double THRESHOLD = 0.000045;
+    private static final double THRESHOLD = 4.5e-5;
 
     public WordChecker() {
         dtw = new DTW();
